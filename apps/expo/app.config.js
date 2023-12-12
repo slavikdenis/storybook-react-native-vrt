1c0934a0c95efac717571b273893d60061da162b
Denis Slavik <denis.slavik@gmail.com>
2023-12-12 17:06:49 +0100

    feat: prettify js files

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -1,8 +1,8 @@
 export default ({ config }) => ({
-	...config,
-	name: 'Expo App',
-	version: '1.0.0',
-	slug: 'expo-app',
-	orientation: 'portrait',
-	userInterfaceStyle: 'light',
+  ...config,
+  name: "Expo App",
+  version: "1.0.0",
+  slug: "expo-app",
+  orientation: "portrait",
+  userInterfaceStyle: "light",
 });
--- packages/tools/eslint-config/react-native.js
+++ packages/tools/eslint-config/react-native.js
@@ -1,6 +1,6 @@
-const { resolve } = require('node:path');
+const { resolve } = require("node:path");
 
-const project = resolve(process.cwd(), 'tsconfig.json');
+const project = resolve(process.cwd(), "tsconfig.json");
 
 /*
  * This is a custom ESLint configuration for use with
@@ -14,23 +14,23 @@ const project = resolve(process.cwd(), 'tsconfig.json');
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-	extends: [
-		'eslint:recommended',
-		'prettier',
-		'eslint-config-turbo',
-		'@react-native-community',
-	],
-	plugins: ['only-warn'],
-	settings: {
-		'import/resolver': {
-			typescript: {
-				project,
-			},
-		},
-	},
-	ignorePatterns: ['node_modules/', 'dist/'],
-	overrides: [
-		// Force ESLint to detect .tsx files
-		{ files: ['*.js?(x)', '*.ts?(x)'] },
-	],
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "eslint-config-turbo",
+    "@react-native-community",
+  ],
+  plugins: ["only-warn"],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ["node_modules/", "dist/"],
+  overrides: [
+    // Force ESLint to detect .tsx files
+    { files: ["*.js?(x)", "*.ts?(x)"] },
+  ],
 };
